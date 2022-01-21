210ba364e209178802f27c00da978a0a736f02ef
fady-farid <60922126+fady-farid@users.noreply.github.com>
2022-01-21 18:02:56 +0200

    add pro

---
+++ omniNotes/build/generated/source/buildConfig/androidTest/alpha/debug/it/feio/android/omninotes/test/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.feio.android.omninotes.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "it.feio.android.omninotes.alpha.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "alpha";
+}
